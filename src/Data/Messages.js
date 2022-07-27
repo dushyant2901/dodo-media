12840b7d9f33b9c29345bc22febcd671cc1c7aee
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-07-27 23:30:30 +0530

    add dummy data

---
+++ src/Data/Messages.js
@@ -0,0 +1,11 @@
+import img1 from "../images/img1.png";
+import img2 from "../images/img2.png";
+import img3 from "../images/img3.png";
+import img4 from "../images/img4.jpg";
+
+export const Messages = [
+  { name: "Andrew Thomas", img: img1, msg: "Just do it bruh" },
+  { name: "Hulk Buster", img: img2, msg: "Just do it bruh" },
+  { name: "Thor", img: img3, msg: "Just do it bruh" },
+  { name: "Natasha", img: img4, msg: "Just do it bruh" },
+];
